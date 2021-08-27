3c7bf38400f39a801b77df7d9c924af8a711b6e8
shivanigoyal1799 <shivanigoyal.mca19.du@gmail.com>
2021-08-27 16:39:13 +0530

    installed styled-components and created styled search cards

--- src/components/Actor/ActorGrid.js
+++ src/components/Actor/ActorGrid.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ActorCard from './ActorCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 const ActorGrid = ({ result }) => {
-  return result.map(({ person }) => (
-    <ActorCard
-      key={person.id}
-      id={person.id}
-      name={person.name}
-      country={person.country}
-      gender={person.gender}
-      birthday={person.birthday}
-      deathday={person.deathday}
-      image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
-    />
-  ));
+  return (
+    <FlexGrid>
+      {result.map(({ person }) => (
+        <ActorCard
+          key={person.id}
+          id={person.id}
+          name={person.name}
+          country={person.country}
+          gender={person.gender}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          image={person.image ? person.image.medium : IMAGE_NOT_FOUND}
+        />
+      ))}
+    </FlexGrid>
+  );
 };
 
 export default ActorGrid;
--- src/components/Show/ShowGrid.js
+++ src/components/Show/ShowGrid.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ShowCard from './ShowCard';
+import { FlexGrid } from '../styled';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 
 const ShowGrid = ({ result }) => {
-  return result.map(({ show }) => (
-    <ShowCard
-      key={show.id}
-      id={show.id}
-      name={show.name}
-      summary={show.summary}
-      image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-    />
-  ));
+  return (
+    <FlexGrid>
+      {result.map(({ show }) => (
+        <ShowCard
+          key={show.id}
+          id={show.id}
+          name={show.name}
+          summary={show.summary}
+          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+        />
+      ))}
+    </FlexGrid>
+  );
 };
 
 export default ShowGrid;
