7a97d6b27a03305e6fcea15ed55a4d3bc94212c5
shivanigoyal1799 <shivanigoyal.mca19.du@gmail.com>
2021-09-03 18:48:43 +0530

    used custom-hooks to star shows

--- src/components/Show/ShowGrid.js
+++ src/components/Show/ShowGrid.js
@@ -2,19 +2,33 @@ import React from 'react';
 import ShowCard from './ShowCard';
 import { FlexGrid } from '../styled';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { useShows } from '../../misc/custom-hooks';
 
 const ShowGrid = ({ result }) => {
+  const [starredShows, dispatchStarred] = useShows();
   return (
     <FlexGrid>
-      {result.map(({ show }) => (
-        <ShowCard
-          key={show.id}
-          id={show.id}
-          name={show.name}
-          summary={show.summary}
-          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-        />
-      ))}
+      {result.map(({ show }) => {
+        const isStarred = starredShows.includes(show.id);
+        const onStarClick = () => {
+          if (isStarred) {
+            dispatchStarred({ type: 'REMOVE', showID: show.id });
+          } else {
+            dispatchStarred({ type: 'ADD', showID: show.id });
+          }
+        };
+        return (
+          <ShowCard
+            key={show.id}
+            id={show.id}
+            name={show.name}
+            summary={show.summary}
+            image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+            onStarClick={onStarClick}
+            isStarred={isStarred}
+          />
+        );
+      })}
     </FlexGrid>
   );
 };
